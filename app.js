446db2946863dc07b827ad4100da77572565ae81
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-28 22:06:46 -0700

    updated javascript game logic

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ let scores = [0, 0];
 let roundScore = 0;
 let activePlayer = 0;
 
-let dice = Math.floor(Math.random() * 6) + 1;
+document.querySelector('.dice').style.display = 'none';
 
-document.querySelector(`#current-${activePlayer}`).textContent = dice;
+document.querySelector('.btn-roll').addEventListener('click', function(){
+  let dice = Math.floor(Math.random() * 6) + 1;
+  let diceDom = document.querySelector('.dice');
+  diceDom.style.display = 'block';
+  diceDom.src = `dice-${dice}.png`;
+});
