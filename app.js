fcb8ef74099ebeb43488a9a408afde10e8042d23
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-28 22:10:40 -0700

    set scores in javascript

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ let activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
 document.querySelector('.btn-roll').addEventListener('click', function(){
   let dice = Math.floor(Math.random() * 6) + 1;
   let diceDom = document.querySelector('.dice');
