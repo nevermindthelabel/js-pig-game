8fa814e9c462661b7041bef59afd0f57e1a9c4a6
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-29 19:26:45 -0700

    added function to start game

--- app.js
+++ app.js
@@ -9,16 +9,7 @@ GAME RULES:
 
 */
 
-let scores = [0, 0];
-let roundScore = 0;
-let activePlayer = 0;
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
   let dice = Math.floor(Math.random() * 6) + 1;
@@ -46,6 +37,18 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
   }
 });
 
+function init() {
+  scores = [0, 0];
+  activePlayer = 0;
+  roundScore = 0;
+
+  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+};
+
 function nextPlayer() {
   activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
   roundScore = 0;
@@ -54,4 +57,6 @@ function nextPlayer() {
   document.querySelector(`.player-0-panel`).classList.toggle('active');
   document.querySelector(`.player-1-panel`).classList.toggle('active');
   document.querySelector('.dice').style.display = 'none';
-}
+};
+
+document.querySelector('.btn-new').addEventListener('click', init);
