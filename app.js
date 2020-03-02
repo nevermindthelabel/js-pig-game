bd95353ca0b98dd9bb89096e58c0e0e5fa5919d1
Matt Kilcup <nevermindthelabel@gmail.com>
2020-03-01 17:43:10 -0700

    changed dice check for 1

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     let diceDom = document.querySelector('.dice');
     diceDom.style.display = 'block';
     diceDom.src = `dice-${dice}.png`;
-    if (dice > 1) {
+    if (dice !== 1) {
       roundScore += dice;
       document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
     } else {
