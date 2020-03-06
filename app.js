1dfeec13875967d483c994e630863ecc460e125e
Matt Kilcup <nevermindthelabel@gmail.com>
2020-03-05 20:12:26 -0700

    updates with second die

--- app.js
+++ app.js
@@ -18,15 +18,19 @@ init();
 document.querySelector('.btn-roll').addEventListener('click', function () {
   if (gamePlaying) {
     let dice = Math.floor(Math.random() * 6) + 1;
-    let diceDom = document.querySelector('.dice');
+    let dice1 = Math.floor(Math.random() * 6) + 1;
+    let diceDom = document.getElementById('dice1');
+    let diceDom1 = document.getElementById('dice2');
     diceDom.style.display = 'block';
+    diceDom1.style.display = 'block';
     diceDom.src = `dice-${dice}.png`;
+    diceDom1.src = `dice-${dice1}.png`;
     if(dice === 6 && lastRoll === 6) {
       scores[activePlayer] = 0;
       document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer];
       nextPlayer();
-    }else if (dice !== 1) {
-      roundScore += dice;
+    }else if (dice && dice1 !== 1) {
+      roundScore += dice + dice1;
       document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
     } else {
       nextPlayer();
@@ -49,7 +53,8 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
 
     if (scores[activePlayer] >= winningScore) {
       document.querySelector(`#name-${activePlayer}`).textContent = 'Winner!!!!!';
-      document.querySelector('.dice').style.display = 'none';
+      document.getElementById('dice1').style.display = 'none';
+      document.getElementById('dice2').style.display = 'none';
       document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
       document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
       document.querySelector('.btn-roll').style.display = 'none';
@@ -69,7 +74,8 @@ function init() {
 
   document.querySelector('.btn-roll').style.display = 'block';
   document.querySelector('.btn-hold').style.display = 'block';
-  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('dice1').style.display = 'none';
+  document.getElementById('dice2').style.display = 'none';
   document.getElementById('score-0').textContent = '0';
   document.getElementById('score-1').textContent = '0';
   document.getElementById('current-0').textContent = '0';
